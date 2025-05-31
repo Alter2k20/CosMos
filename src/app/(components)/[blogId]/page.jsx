ef4809882551fa113001422eec76bc917d031239
Alter2k20 <mishraashutosh98611@gmail.com>
2025-05-31 07:14:58 +0530

    added some css

--- src/app/(components)/[blogId]/page.jsx
+++ src/app/(components)/[blogId]/page.jsx
@@ -9,11 +9,17 @@ export default async function page({ params }) {
     // console.log(blog);
 
     return (
-        <div className="flex flex-col justify-center place-items-center p-4">
-            <div id="heding">{blog[0].heading}</div>
-            <div id="writer">{blog[1].username}</div>
-            <div id="topic">{blog[0].topic}</div>
-            <MarkdownRenderer content={blog[0].content}></MarkdownRenderer>
+        <div className="flex flex-col items-center p-8 bg-gray-900 min-h-screen">
+            <article className="max-w-3xl w-full bg-gray-800 shadow-md rounded-lg p-6">
+                <header className="mb-6">
+                    <h1 id="heading" className="text-4xl font-bold text-gray-100 mb-2">{blog[0].heading}</h1>
+                    <p id="topic" className="text-lg text-gray-400 bg-gray-900 rounded-2xl p-2 w-fit italic ">{blog[0].topic}</p>
+                    <p id="writer" className="text-md text-gray-500"> by: {blog[1].username}</p>
+                </header>
+                <section>
+                    <MarkdownRenderer content={blog[0].content}></MarkdownRenderer>
+                </section>
+            </article>
         </div>
     );
 }
